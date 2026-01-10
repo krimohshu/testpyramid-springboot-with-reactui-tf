4801bb25087e2ab21d21bd434dfb05692f77cc0d
krishan <krishan.shyam@gmail.com>
2026-01-10 22:27:55 +0000

    fix(infra): use db_name for RDS, fix ecr outputs and remove terraform binaries

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,6 @@
 output "ecr_repository_url" {
-  value = data.aws_ecr_repository.app.repository_uri
+  # data.aws_ecr_repository exports `repository_url` (not repository_uri)
+  value = data.aws_ecr_repository.app.repository_url
 }
 
 output "db_endpoint" {
